8e0189a8644f0782399f0fb7f40a7dd9d27b6fe8
P0S1T1VE GUY <UriyDoroshenko@ya.ru>
2025-08-10 17:05:57 +0300

    making client tunnel

--- user_interface/profile_starter.py
+++ user_interface/profile_starter.py
@@ -1,6 +1,16 @@
 try:
-    import os
-    parameters = ' --'.join(open('profile.pyroxy', 'r').read().split('\n'))
-    os.system('python console_client.py --' + parameters)
+    import os, sys
+
+    target = 'client.exe'
+    if sys.argv[-1] == '--py':
+        target = 'python client.py'
+
+    if os.path.isfile('profile.pyroxy'):
+        print('running with profile config...')
+        parameters = ' --'.join(open('profile.pyroxy', 'r').read().split('\n'))
+        os.system(f'{target} --{parameters}')
+    else:
+        input('ERROR: profile config not found, looking for a "profile.proxy" file. Press ENTER to close the program...')
+
 except KeyboardInterrupt:
     pass
