a4d6fa1649744c69d58cb45c4c0a84c7035c811c
P0S1T1VE GUY <UriyDoroshenko@ya.ru>
2025-09-10 19:45:04 +0300

    deploy update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
 import logging
+import argparse
 from pathlib import Path
 from logging.handlers import TimedRotatingFileHandler
 
@@ -10,8 +11,20 @@ from pyroxy.db_handlers import SQLite_Handler
 from pyroxy.wrappers import HTTP_WS_Wrapper
 
 
-db_file = Path(__file__).parent / 'pyroxy' / "telegram_bot" / "db.sqlite3"
-key_file = Path(__file__).parent / 'pyroxy' / "telegram_bot" / "default_server_key"
+parser = argparse.ArgumentParser(description="PyROXY server starter")
+logging_levels = ['info', 'debug', 'warning', 'error']
+logging_levels += [level.upper() for level in logging_levels]
+parser.add_argument("--logging", choices=logging_levels, default='info', help="logging level")
+parser.add_argument("--hosting", type=int, choices=[1,0], default=1, help="hosting mode")
+args = parser.parse_args()
+
+
+if args.hosting:
+    db_file = Path(__file__).parent / 'pyroxy' / "telegram_bot" / "db.sqlite3"
+    key_file = Path(__file__).parent / 'pyroxy' / "telegram_bot" / "default_server_key"
+else:
+    db_file = Path(__file__).parent / "telegram_bot" / "db.sqlite3"
+    key_file = Path(__file__).parent / "telegram_bot" / "default_server_key"
 log_file = Path(__file__).parent / 'logs' / "proxy.log"
 
 
@@ -43,7 +56,10 @@ SERVER = Socks5Server(
     db_handler=SQLite_Handler(filepath=db_file),
     ciphers=available_ciphers,
     udp_cipher=available_ciphers[2],
-    port=180
+    port=80
 )
+
+SERVER.logger.setLevel(getattr(logging, args.logging.upper(), logging.INFO))
+
 SERVER.logger.addHandler(file_handler)
 asyncio.run(SERVER.start())
