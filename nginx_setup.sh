7bac08870ae0a9f33f947bb23f861ef854087e8a
P0S1T1VE GUY <UriyDoroshenko@ya.ru>
2025-09-24 13:45:26 +0300

    nginx update

---
+++ nginx_setup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -e
+
+echo "[*] installing nginx..."
+sudo apt update -y
+sudo apt install -y nginx
+sudo apt-get install libnginx-mod-stream
+
+echo "[*] making proxy.conf..."
+cat << 'EOF' | sudo tee /etc/nginx/nginx.conf > /dev/null
+load_module /usr/lib/nginx/modules/ngx_stream_module.so;
+
+user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
+
+events {
+    worker_connections 1024;
+}
+
+stream {
+    log_format basic '$remote_addr [$time_local] '
+                     '$protocol $status $bytes_sent $bytes_received '
+                     '$session_time';
+
+    limit_conn_zone $binary_remote_addr zone=addr:10m;
+
+    upstream backend {
+        server 127.0.0.1:8080;
+    }
+
+    server {
+        listen 80;
+
+        # Ограничения
+        proxy_connect_timeout 5s;
+        proxy_timeout 240s;
+
+        # Количество соединений
+        limit_conn addr 300;
+
+        # Логи
+        access_log /var/log/nginx/proxy_access.log basic;
+        error_log /var/log/nginx/proxy_error.log warn;
+
+        proxy_pass backend;
+    }
+}
+EOF
+
+sudo update-alternatives --config python3
+echo "[*] Enabling config..."
+sudo nginx -t
+
+echo "[*] Restarting nginx..."
+sudo systemctl restart nginx
+
+sudo update-alternatives --config python3
+echo "[+] Done!"
